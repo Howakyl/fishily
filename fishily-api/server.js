587d5b73af824867c8bd303e13ecb988f9f0c069
Howakyl96 <howakyl@gmail.com>
2020-11-28 15:44:35 -0800

    initalized server.js boilerplate

---
+++ fishily-api/server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+require('dotenv').config();
+// const cors = require('cors');
+const routes = require('./routes');
+
+const port = process.env.PORT || 4000;
+const app = express();
+
+// const corsOptions = {
+//     origin: 'http://localhost:3000'
+// }
+
+app.use(express.json());
+// app.use(cors(corsOptions));
+
+//API ROUTES
+
+
+//conection 
+app.listen(port, () => console.log(`Server is running on port ${port}`));
