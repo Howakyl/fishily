984cab7a0562a3806d10bf5188c573675f1a2590
Howakyl96 <howakyl@gmail.com>
2020-11-30 15:53:05 -0800

    navbar links to home and users page

--- fishily-client/src/config/routes.js
+++ fishily-client/src/config/routes.js
@@ -2,9 +2,11 @@ import React from 'react';
 import { Switch , Route } from 'react-router-dom';
 
 import Home from '../pages/Home';
+import UserList from '../pages/UserList';
 
 export default (
     <Switch>
         <Route exact path='/' component={ Home }/>
+        <Route path='/users' component={ UserList }/>
     </Switch>
 )
--- fishily-client/src/pages/Home.js
+++ fishily-client/src/pages/Home.js
@@ -1,11 +1,9 @@
 import React from 'react';
-import UserList from './UserList';
 
 const Home = () => {
     return (
         <div>
             <h1>Welcome to Fishily!</h1>
-            <UserList />
         </div>
     );
 };
