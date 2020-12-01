793d4eddc4040df58a31c216547402401cfb0432
Howakyl96 <howakyl@gmail.com>
2020-12-01 15:35:36 -0800

    refactored form component

--- fishily-client/src/pages/SignUp.js
+++ fishily-client/src/pages/SignUp.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import UserModel from '../models/user';
+// import UserModel from '../models/user';
 
 class SignUp extends React.Component {
     state = {
@@ -14,7 +14,30 @@ class SignUp extends React.Component {
     render () {
         return (
             <div>
-                <h1>Sign Up!</h1>
+                <form className="container">
+                    <h1>Sign Up!</h1>
+                    <div className="form-group">
+                        <label htmlFor="usernameInput">username</label>
+                        <small className="form-text text-muted">required</small>
+                        <input type="text" className="form-control" id="usernameInput" aria-describedby="emailHelp"/>
+                    </div>
+                    <div className="form-group">
+                        <label htmlFor="passInput">Password</label>
+                        <small className="form-text text-muted">required</small>
+                        <input type="password" className="form-control" id="passInput"/>
+                        <small id="emailHelp" className="form-text text-muted">don't use a sensitive password!</small>
+                    </div>
+                    <div className="form-group">
+                        <label htmlFor="firstNameInput">first name</label>
+                        <input type="text" className="form-control" id="firstNameInput" aria-describedby="emailHelp"/>
+                    </div>
+                    <div className="form-group">
+                        <label htmlFor="lastNameInput">last name</label>
+                        <input type="text" className="form-control" id="lastNameInput" aria-describedby="emailHelp"/>
+                    </div>
+                    
+                    <button type="submit" className="btn btn-primary">Submit</button>
+                </form>
             </div>
         )
     }
