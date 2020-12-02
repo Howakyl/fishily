bfc62e84013d4b5256ea97a84cc0cf7f25837158
Howakyl96 <howakyl@gmail.com>
2020-12-01 17:05:44 -0800

    auth server config

--- fishily-api/server.js
+++ fishily-api/server.js
@@ -2,6 +2,7 @@ const express = require("express");
 require('dotenv').config();
 const cors = require('cors');
 const routes = require('./routes');
+const session = require('express-session');
 
 const port = process.env.PORT || 4000;
 const app = express();
@@ -12,6 +13,16 @@ const corsOptions = {
 
 app.use(express.json());
 app.use(cors(corsOptions));
+
+//EXPRESS SESSION
+app.use(session({
+    secret: process.env.SESSION_SECRET,
+    resave: false, 
+    saveUninitialized: false,
+    cookie: {
+        maxAge: 1000 * 60 * 60 * 24 * 7 * 2
+    }
+}));
 
 //API ROUTES
 app.use("/api/fishily/users", routes.users);
