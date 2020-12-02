3c51f632ab2f7893dc33123bec7c58ec5724f0ef
Howakyl96 <howakyl@gmail.com>
2020-12-01 16:22:26 -0800

    users can now be created from sign up page

--- fishily-client/src/pages/SignUp.js
+++ fishily-client/src/pages/SignUp.js
@@ -1,5 +1,5 @@
 import React from 'react';
-// import UserModel from '../models/user';
+import UserModel from '../models/user';
 
 class SignUp extends React.Component {
     state = {
@@ -8,7 +8,6 @@ class SignUp extends React.Component {
         lastName: '',
         password: '',
         bio: '',
-        picture: ''
     };
 
     handleInputchamge = (event) => {
@@ -17,10 +16,19 @@ class SignUp extends React.Component {
         this.setState({ [event.target.name] : event.target.value });
     };
 
+    handleFormSubmit = (event) => {
+        event.preventDefault();
+
+        UserModel.create(this.state)
+        .then((res) => {
+            this.props.history.push('/');
+        })
+    }
+
     render () {
         return (
             <div>
-                <form className="container">
+                <form className="container" onSubmit={this.handleFormSubmit}>
                     <h1>Sign Up!</h1>
                     <div className="form-group">
                         <label htmlFor="usernameInput">username</label>
@@ -30,30 +38,58 @@ class SignUp extends React.Component {
                             type="text" 
                             className="form-control" 
                             id="usernameInput"
+                            value={this.state.username}
                             name="username"
                         />
                     </div>
                     <div className="form-group">
                         <label htmlFor="passInput">Password</label>
-                        <small className="form-text text-muted">required</small>
-                        <input type="password" className="form-control" id="passInput"/>
-                        <small id="emailHelp" className="form-text text-muted">don't use a sensitive password!</small>
+                        <small className="form-text text-muted">required - Don't use a sensitive password!</small>
+                        <input
+                            onChange={this.handleInputchamge}
+                            type="password" 
+                            className="form-control" 
+                            id="passInput"
+                            value={this.state.password}
+                            name="password"
+                        />
+                        {/* <small id="emailHelp" className="form-text text-muted">don't use a sensitive password!</small> */}
                     </div>
                     <div className="form-group">
                         <label htmlFor="firstNameInput">first name</label>
-                        <input type="text" className="form-control" id="firstNameInput" />
+                        <input
+                            onChange={this.handleInputchamge}
+                            type="text" 
+                            className="form-control" 
+                            id="firstNameInput"
+                            value={this.state.firstName}
+                            name="firstName" 
+                        />
                     </div>
                     <div className="form-group">
                         <label htmlFor="lastNameInput">last name</label>
-                        <input type="text" className="form-control" id="lastNameInput" />
+                        <input
+                            onChange={this.handleInputchamge}
+                            type="text" 
+                            className="form-control" 
+                            id="lastNameInput" 
+                            value={this.state.lastName}
+                            name="lastName"
+                        />
                     </div>
                     <div className="form-group">
                         <label htmlFor="bioInput">Create a bio:</label>
-                        <textarea type="text" className="form-control" id="bioInput" />
+                        <textarea
+                            onChange={this.handleInputchamge}
+                            type="text" 
+                            className="form-control" 
+                            id="bioInput" 
+                            value={this.state.bio}
+                            name="bio"
+                        />
                     </div>
 
-                    
-                    <button type="submit" className="btn btn-primary">Submit</button>
+                    <button type="submit" className="btn btn-primary">Create Account</button>
                 </form>
             </div>
         )
