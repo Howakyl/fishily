266413837815d07d0c04aebbf13e7284359397a3
Howakyl96 <howakyl@gmail.com>
2020-12-02 20:40:35 -0800

    all posts request on post list

--- fishily-client/src/pages/PostList.js
+++ fishily-client/src/pages/PostList.js
@@ -1,17 +1,25 @@
 import React from 'react';
 import PostCard from '../components/PostCard';
+import PostModel from '../models/post';
 
 class PostList extends React.Component {
 
-    // state = {
-    //     posts: [],
-    //     loading: true,
-    // }
+    state = {
+        posts: [],
+        loading: true,
+    }
 
-    // componentDidMount() {
+    componentDidMount() {
 
+        PostModel.all().then((res) => {
+            console.log('data: ', res);
 
-    // }
+            this.setState({
+                posts: res.data.posts,
+                loading: false,
+            });
+        });
+    };
 
     render () {
         return (
