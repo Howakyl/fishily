1dfaa21a61484165bd90280a8e8cd3bde1d98335
Howakyl96 <howakyl@gmail.com>
2020-12-02 20:18:36 -0800

    added new post to nav if loggedin

--- fishily-client/src/components/Navbar.js
+++ fishily-client/src/components/Navbar.js
@@ -16,7 +16,7 @@ const Navbar = (props) => {
         if(!props.user.username) {
             return (
                 <>
-                    <li className="nav-item active">
+                    <li >
                         <Link className="nav-link" to="/login">Log In</Link>
                     </li>
                     <li className="nav-item active">
@@ -35,6 +35,18 @@ const Navbar = (props) => {
         }
     }
 
+    function renderCreatePost() {
+        if(props.user.username) {
+            return (
+                <>
+                    <li className="nav-item active">
+                        <Link className="nav-link" to='/posts/new'>New Post</Link>
+                    </li>
+                </>
+            )
+        }
+    }
+
     console.log(loginNav())
     return (
         <nav className="navbar navbar-expand-md navbar-dark bg-dark">
@@ -48,19 +60,8 @@ const Navbar = (props) => {
                 <li className="nav-item active">
                     <Link className="nav-link" to="/users">All users</Link>
                 </li>
-                
+                {renderCreatePost()}
                 {loginNav()}
-                {/* <li className="nav-item active">
-                    <Link className="nav-link" to="/login">Log In</Link>
-                </li> */}
-                {/* <li className="nav-item dropdown">
-                    <a className="nav-link dropdown-toggle" href="#" id="dropdown04" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Dropdown</a>
-                    <div className="dropdown-menu" aria-labelledby="dropdown04">
-                        <a className="dropdown-item" href="#">Action</a>
-                        <a className="dropdown-item" href="#">Another action</a>
-                        <a className="dropdown-item" href="#">Something else here</a>
-                    </div>
-                </li> */}
                 </ul>
                 {/* <form className="form-inline my-2 my-md-0">
                 <input className="form-control" type="text" placeholder="Search"/>
