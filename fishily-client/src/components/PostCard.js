8b4ffdca18f92752e621c56de66d2d231899c7bb
Howakyl96 <howakyl@gmail.com>
2020-12-03 17:08:32 -0800

    almost have details working

--- fishily-client/src/components/PostCard.js
+++ fishily-client/src/components/PostCard.js
@@ -2,17 +2,20 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 const PostCard  = (props) => {
+    
     return (
         
         <div className="card mb-3" style={{maxWidth: "540px"}}>
             <div className="row no-gutters">
                 <div className="col-md-4">
-                    <img src={props.post.image} className="card-img" alt="..."/>
+                    <img src={props.post.image} className="card-img" alt="post header"/>
                 </div>
                 <div className="col-md-8">
                     <div className="card-body">
+                        {/* <Link to={`posts/${props.post._id}`} className="card-title">{props.post.title}</Link> */}
                         <Link to={`posts/${props.post._id}`} className="card-title">{props.post.title}</Link>
-                        <h6>By: {props.post.user.username}</h6>
+                        {/* <h6>By: {props.post}</h6> */}
+                        
                         <p className="card-text text-truncate">{props.post.description}</p>
                         <p className="card-text"><small className="text-muted">Posted On: {props.post.date}</small></p>
                     </div>
--- fishily-client/src/pages/PostDetail.js
+++ fishily-client/src/pages/PostDetail.js
@@ -1,23 +1,44 @@
 import React from 'react'; 
+import PostModel from '../models/post';
+import PostCard from '../components/PostCard';
 
 class PostDetail extends React.Component {
     state = {
-        title : '',
-        description: '',
-        fish: '',
-        location: {
-            name: '',
-            lat: null,
-            lng: null,
-        },
-        image: ''
+        // title : '',
+        // description: '',
+        // fish: '',
+        // location: {
+        //     name: '',
+        //     lat: null,
+        //     lng: null,
+        // },
+        // image: ''
+        post: {}
+    }
+
+    //REFERENCE PROPS FOR USER INFO!
+
+    componentDidMount() {
+        const postId = this.props.match.params.id;
+        // console.log(postId)
+        // console.log(this.props)
+
+        PostModel.getOne(postId)
+            .then((data) => {
+                // console.log('DETAIL RES: ', res);
+
+                this.setState({ 
+                    post : data.post
+                })
+                console.log('after :' , this.state)
+            })
     }
 
     render () {
         console.log('post detail props:',this.props)
             return (
                 <div>
-                    hi
+                    <PostCard post={this.state.post}/>
                 </div>
             )
     }
