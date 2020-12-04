9a9b6bc52a62d3392e64f3fbdce063bb391ac0e3
Howakyl96 <howakyl@gmail.com>
2020-12-03 20:04:00 -0800

    editpost state has access to post data

--- fishily-client/src/pages/EditPost.js
+++ fishily-client/src/pages/EditPost.js
@@ -1,12 +1,41 @@
 import React from 'react';
+import PostModel from '../models/post';
+import { Redirect } from 'react-router-dom';
 
-const EditPost = (props) => {
-    return (
+class EditPost extends React.Component {
+    state = {
+        title : '',
+        description: '',
+        fish: '',
+        location: {
+            name: '',
+            lat: null,
+            lng: null,
+        },
+        image: '',
+        post: {},
+        redirectToPosts: false
+    }
 
-    <div>
-        <h1>HELLLOOOO</h1>
-    </div>
-    )
+    componentDidMount() {
+        const postId = this.props.match.params.id;
+        PostModel.getOne(postId)
+            .then((data) => {
+                this.setState({ 
+                    post: data.data.post,
+                })
+            })
+    };
+    
+    render() {
+        console.log(this.state)
+        return (
+    
+        <div>
+            <h1>HELLLOOOO</h1>
+        </div>
+        )
+    }
 }
 
 export default EditPost;
