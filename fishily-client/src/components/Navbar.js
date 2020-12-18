81a93925c45c6275f1e77cc802d4d8b45288d243
Howakyl96 <howakyl@gmail.com>
2020-12-18 12:58:53 -0800

    logged in user img on nav, links to user detail

--- fishily-client/src/components/Navbar.js
+++ fishily-client/src/components/Navbar.js
@@ -72,6 +72,9 @@ const Navbar = (props) => {
                 {/* <form className="form-inline my-2 my-md-0">
                 <input className="form-control" type="text" placeholder="Search"/>
                 </form> */}
+                <Link to={`users/${props.user._id}`}>
+                    <img src={props.user.picture} alt="" className="navbar-user-img"/>
+                </Link>
             </div>
         </nav>
     )
