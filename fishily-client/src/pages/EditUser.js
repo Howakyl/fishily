9ab02c68e5b6079f4f1a3e816ca2b584cf7f8eb8
Howakyl96 <howakyl@gmail.com>
2020-12-24 15:23:32 -0800

    editUser username input can be changed

--- fishily-client/src/pages/EditUser.js
+++ fishily-client/src/pages/EditUser.js
@@ -27,24 +27,44 @@ const EditUser = (props) => {
     } , [props.match.params.id]);
 
     const handleInputChange = (event) => {
+        console.log('input changed : ' , event.target.name)
         setFormInput({ [event.target.name] : event.target.value });
+        
     }
 
     const handleFormSubmit = (event) => {
         event.preventDefault();
 
         const userId = props.match.params.id;
-        UserModel.update(userId, formInput)
+        UserModel.update(userId, (username, firstName, lastName, bio, picture))
             .then((res) => {
                 console.log('updated user: ', res);
                 this.props.history.push(`/users/${userId}`);
             });
     };
 
-    return (
-        <>
-        </>
-    )
+    if (!isLoading) {
+        return (
+            <div>
+                <form onSubmit={handleFormSubmit}>
+                    <h1>Edit your profile!</h1>
+                    <div className="form-group">
+                        <label htmlFor="usernameInput">Username:</label>
+                        <input
+                            onChange={e => setUsername(e.target.value)}
+                            type="text" 
+                            className="form-control" 
+                            id="usernameInput"
+                            value={username}
+                            name="username"
+                        />
+                    </div>
+                </form>
+            </div>
+        )
+    } else {
+        return <h3>Loading...</h3>
+    }
 }
 
 export default EditUser;
