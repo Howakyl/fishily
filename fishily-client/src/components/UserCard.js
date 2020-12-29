3f4d316abc83744dfd60550603ebe06a1096f1e5
Howakyl96 <howakyl@gmail.com>
2020-12-28 22:35:01 -0800

    centered username p on post detail

--- fishily-client/src/components/UserCard.js
+++ fishily-client/src/components/UserCard.js
@@ -12,11 +12,10 @@ const UserCard = (props) => {
     }
 
     return (
-        
         <div className="card" style={{width: '18rem'}}>
             <img src={props.user.picture} className="card-img-top" alt='user'/>
             <div className="card-body">
-                <h5 className="card-title">{props.user.username}</h5>
+                <h5 className="card-title text-truncate">{props.user.username}</h5>
                 <div className="card-text text-truncate">{bioText()}</div>
                 <Link to={`users/${props.user._id}`} className="btn btn-primary">View Profile</Link>
             </div>
