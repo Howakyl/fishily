fab318a577fa72fa5d38e1ff07b7893fddf1fa0d
Howakyl96 <howakyl@gmail.com>
2020-12-28 21:23:57 -0800

    welcome msg on home appears only if logged in

--- fishily-client/src/pages/Home.js
+++ fishily-client/src/pages/Home.js
@@ -1,10 +1,19 @@
 import React from 'react';
 
 const Home = (props) => {
+
+    function welcomeMsg () {
+        if (props.user.username) {
+            <>
+                <p>Logged in as: {props.user.username}</p>
+            </>
+        }
+    }
+
     return (
         <div className=" home-container">
             <h1>Welcome to Fishily!</h1>
-            <p>Logged in as: {props.user.username}</p>
+            {welcomeMsg()}
         </div>
     );
 };
