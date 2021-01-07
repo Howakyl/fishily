445264bff5a4a0779280ead5d50635e5da224502
Howakyl96 <howakyl@gmail.com>
2021-01-06 17:39:48 -0800

    profile img on hero

--- fishily-client/src/pages/Home.js
+++ fishily-client/src/pages/Home.js
@@ -2,15 +2,15 @@ import React from 'react';
 
 const Home = (props) => {
 
-    function welcomeMsg () {
-        if (props.user.username) {
-            return (
-                <>
-                    <p>Logged in as: {props.user.username}</p>
-                </>
-            )
-        }
-    }
+    // function welcomeMsg () {
+    //     if (props.user.username) {
+    //         return (
+    //             <>
+    //                 <p>Logged in as: {props.user.username}</p>
+    //             </>
+    //         )
+    //     }
+    // }
 
     return (
         <div>
@@ -22,12 +22,16 @@ const Home = (props) => {
             <section id="hero-section" className="hero">
                 <h2>Connecting fishermen around the <span className="hero-emphasis">world</span>.</h2>
                 <div className="hero-card">
-                    <img className="img-fluid hero-img" src="/hero-images/post-card.png" alt=""/>
-                    <h3>Show off those <span className="hero-emphasis">big</span> catches.</h3>
+                    <h3>Find your new <span className="hero-emphasis">Favorite</span> fishing hole!</h3>
+                    <img src="/hero-images/map.png" className="img-fluid hero-img" alt="map"/>
                 </div>
                 <div className="hero-card">
-                    <h3>Find your new favorite fishing hole.</h3>
-                    <img src="/hero-images/map.png" className="img-fluid hero-img" alt=""/>
+                    <img className="img-fluid hero-img" src="/hero-images/post-card.png" alt="post"/>
+                    <h3>Show off those <span className="hero-emphasis">Big</span> catches.</h3>
+                </div>
+                <div className="hero-card">
+                    <h3>Personalize your profile!</h3>
+                    <img src="/hero-images/profile2.png" alt="profile page" className="img-fluid profile-img"/>
                 </div>
             </section>
         </div>
